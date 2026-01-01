acaf58546d03ae248946e9f6bb7a4a6ffa645ac1
Evansrintari <rintari110@gmail.com>
2026-01-01 11:35:43 +0300

    changing things

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,22 +2,39 @@ import { ClerkProvider } from "@clerk/clerk-expo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import * as SecureStore from "expo-secure-store";
 import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { AppContext } from "../contexts/AppContext";
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import '../global.css';
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 SplashScreen.preventAutoHideAsync();
 
-
-{/* */}
 const queryClient = new QueryClient();
 
 function RootLayoutNav() {
   return (
-    <ClerkProvider  tokenCache={tokenCache}>
-    <Slot/>
+    <ClerkProvider 
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY} 
+      tokenCache={tokenCache}
+    >
+      <Slot />
     </ClerkProvider>
   );
 }
